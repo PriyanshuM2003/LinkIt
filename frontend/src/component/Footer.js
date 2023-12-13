f2a6151de435b0392d74d922dc401cacfafca147
PriyanshuM2003 <studyx222@gmail.com>
2023-12-13 19:17:10 +0530

    contact modal added

--- frontend/src/component/Footer.js
+++ frontend/src/component/Footer.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
 import Link from "@material-ui/core/Link";
 import Grid from "@material-ui/core/Grid";
+import Modal from "@material-ui/core/Modal";
 
 const useStyles = makeStyles((theme) => ({
   footer: {
@@ -11,10 +12,38 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: "#401d1d",
     color: theme.palette.primary.contrastText,
   },
+  modal: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  modalContent: {
+    backgroundColor: "#401d1d",
+    color: "#fff",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+  link: {
+    textDecoration: "none",
+    color: "inherit",
+    "&:hover": {
+      textDecoration: "none",
+      color: "red",
+    },
+  },
 }));
 
 function Footer() {
   const classes = useStyles();
+  const [openModal, setOpenModal] = useState(false);
+
+  const handleOpenModal = () => {
+    setOpenModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setOpenModal(false);
+  };
 
   return (
     <footer className={classes.footer}>
@@ -33,9 +62,9 @@ function Footer() {
           <Grid item xs={12} sm={6}>
             <Typography variant="body2" align="right">
               <Link
-                href="mailto:tester146924@gmail.com"
-                target="_blank"
+                style={{ cursor: "pointer" }}
                 color="inherit"
+                onClick={handleOpenModal}
               >
                 Contact Us
               </Link>
@@ -54,6 +83,26 @@ function Footer() {
             </Typography>
           </Grid>
         </Grid>
+        <Modal
+          className={classes.modal}
+          open={openModal}
+          onClose={handleCloseModal}
+        >
+          <div className={classes.modalContent}>
+            <Typography variant="h6">Contact Us</Typography>
+            <Typography>
+              You can contact us at:{" "}
+              <Link
+                href="mailto:tester146924@gmail.com"
+                target="_blank"
+                color="inherit"
+                className={classes.link}
+              >
+                tester146924@gmail.com
+              </Link>
+            </Typography>
+          </div>
+        </Modal>
       </Container>
     </footer>
   );
